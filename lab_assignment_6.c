980137ddd05692e4514e4d04ac3ed663ab9281a0
MorganGravelat <mgravelat@gmail.com>
2024-02-23 11:10:01 -0500

    Mostly finished lab 6 assignment

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int search(int numbers[], int low, int high, int value) 
-{
-	return -1;
+int search(int numbers[], int low, int high, int value) {
+    if (high >= low) { //If the count of high value is currently greater than or equal to the lower value keep going until it is either found or not there
+        int mid = low + (high - low) / 2; //Taking the middle value of the number array
+
+        // If the element is in the middle already
+        if (numbers[mid] == value) return mid;
+
+        // Since the numbers are already assorted if the value is less than the numbers midway through you only search left
+        if (numbers[mid] > value) return search(numbers, low, mid - 1, value);
+
+        // Sasme concept as above but only search the right since it is higher.
+        return search(numbers, mid + 1, high, value);
+    }
+
+    // The value was not found
+    return -1;
 }
 
 void printArray(int numbers[], int sz)
@@ -28,7 +42,7 @@ int main(void)
 	FILE* inFile = fopen("input.txt","r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &countOfNums);
